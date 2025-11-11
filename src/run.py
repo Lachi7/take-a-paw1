44b59e9ee8ea49c9941b72ed0f1fde8ecd8cb8ec
shahin1717 <sahinalekperov5@gmail.com>
2025-11-11 11:28:53 +0400

    some changes to code for initializing db

--- src/run.py
+++ src/run.py
@@ -1,8 +1,12 @@
 # run.py
 from app import create_app
+from app.db import db
 
 if __name__ == "__main__":
     app = create_app()
+    with app.app_context():
+        db.create_all()  # Creates all tables defined in your models
+        print("✅ Tables created (if they didn't exist)")
     print("🎯 Starting Flask development server...")
     print("🌐 App at http://localhost:5000")
     app.run(host="0.0.0.0", port=5000, debug=True)
